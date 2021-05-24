e29e7486a1f00bb202683684964425600a7e2e37
myGit@git.com <myGit@git.com>
2021-05-24 15:07:38 +0600

    App admin.py diplayed links on amdin side customized

--- wppApp/admin.py
+++ wppApp/admin.py
@@ -5,6 +5,7 @@ admin.site.register(Site)
 class PageAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'uri', 'site')
     list_filter = ('site',)
+    list_display_links = ('id', 'title')
     list_per_page = 50
 
 admin.site.register(Page, PageAdmin)
@@ -14,6 +15,7 @@ admin.site.register(PageImages)
 class ExceptedPageAdmin(admin.ModelAdmin):
     list_display = ('id','uri', 'site')
     list_filter = ('site',)
+    list_display_links = ('id', 'site')
     list_per_page = 50
 
 admin.site.register(ExceptedPages, ExceptedPageAdmin)
